1e6da54f2ce5285fefc554ab8abd548a15ae6b87
Guy Indepurker <guy235@gmail.com>
2020-11-26 03:33:09 +0200

    add some

--- src/cmps/board-list.vue
+++ src/cmps/board-list.vue
@@ -1,64 +1,56 @@
 <template>
-    <section class="board-list flex column">
-        <div class="top-bar flex space-around align-center">
-            <h1 class="title-board-list">
-                Boards 
-            </h1>
-            <div  @click="addBoard">
-            <i class="btn-add fas fa-plus-circle"></i>
-            </div>
-        </div>
-            <input class="board-list-search"
-                type="text"
-                @input="emitSearch"
-                v-model="searchBoard"
-                placeholder="Search"
-            />
+  <section class="board-list flex column">
+    <div class="top-bar flex space-around align-center">
+      <h1 class="title-board-list">Boards</h1>
+        <i @click="addBoard" class="btn-add fas fa-plus-circle"></i>
+    </div>
+    <input
+      class="board-list-search"
+      type="text"
+      @input="emitSearch"
+      v-model="searchBoard"
+      placeholder="Search"
+    />
 
-        <ul v-if="boards" class="boards-list clean-list">
-            <li class="board-item" v-for="board in boards" :key="board._id">
-<div @click="removeCurrBoard(board._id)">
-  
-<i class="trash-icon fas fa-trash"></i>
-</div>
-                <router-link  :to="'/board/' + board._id">{{
-                    board.name
-                }}</router-link>
-            </li>
-        </ul>
-    </section>
+    <ul v-if="boards" class="boards-list clean-list">
+      <li class="board-item" v-for="board in boards" :key="board._id">
+          
+          <i @click="removeCurrBoard(board._id)" class="trash-icon fas fa-trash"></i>
+        <router-link :to="'/board/' + board._id">{{ board.name }}</router-link>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
 import lodash from 'lodash'
 
 export default {
-    name: 'board-list',
-    props: {
-        boards: [Array, Object],
+  name: 'board-list',
+  props: {
+    boards: [Array, Object],
+  },
+  data() {
+    return {
+      searchBoard: '',
+    }
+  },
+  computed: {},
+  methods: {
+    emitSearch() {
+      const searchBoard = JSON.parse(JSON.stringify(this.searchBoard))
+      this.$emit('searchBoard', searchBoard)
     },
-    data() {
-        return {
-            searchBoard: '',
-        }
+    removeCurrBoard(boardId) {
+      console.log('boardId:', boardId)
+      this.$emit('removeBoard', boardId)
     },
-    computed: {},
-    methods: {
-        emitSearch() {
-            const searchBoard = JSON.parse(JSON.stringify(this.searchBoard))
-            this.$emit('searchBoard', searchBoard)
-        },
-        removeCurrBoard(boardId) {
-            console.log('boardId:', boardId)
-            this.$emit('removeBoard', boardId)
-        },
-        addBoard() {
-        
-            this.$emit('addNewBoard')
-        },
+    addBoard() {
+      this.$emit('addNewBoard')
     },
-    created() {
-        this.emitSearch = lodash.debounce(this.emitSearch, 500)
-    },
+  },
+  created() {
+    this.emitSearch = lodash.debounce(this.emitSearch, 500)
+  },
 }
 </script>
--- src/cmps/group-list.vue
+++ src/cmps/group-list.vue
@@ -2,13 +2,13 @@
   <section v-if="groups.length" class="group-list">
     <h2>Board: {{ boardName }}</h2>
     <ul class="clean list flex column align-center justify-center gap">
-      <group-preview v-for="group in groups" :key="group._id" :group="group" />
+      <!-- <group-preview v-for="group in groups" :key="group._id" :group="group" /> -->
     </ul>
   </section>
 </template>
 
 <script>
-import groupPreview from "@/cmps/group-preview.vue";
+// import groupPreview from "@/cmps/group-preview.vue";
 
 export default {
   name: "group-list",
@@ -17,7 +17,7 @@ export default {
     boardName: String,
   },
   components: {
-    groupPreview,
+    // groupPreview,
   },
 };
 </script>
