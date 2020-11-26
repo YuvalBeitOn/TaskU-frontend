ec6add4fb94486279e17a54b6077e89b826a9e0c
Orel Vaizman <orelvaizman@h-MacBook-Pro-sl-Orel.local>
2020-11-26 03:40:04 +0200

    update

--- src/cmps/group-list.vue
+++ src/cmps/group-list.vue
@@ -1,15 +1,8 @@
 <template>
-<<<<<<< HEAD
-  <section v-if="groups.length" class="group-list">
-    <h2>Board: {{ boardName }}</h2>
-    <ul class="clean list flex column align-center justify-center gap">
-      <!-- <group-preview v-for="group in groups" :key="group._id" :group="group" /> -->
-=======
   <section v-if="groups.length" class="group-list container">
     <h2>{{ boardName }}</h2>
     <ul class="clean-list flex column align-center justify-center gap width100">
       <group-preview v-for="group in groups" :key="group._id" :group="group" />
->>>>>>> 897b84bf765fb1b3402272d356b3c12e721bb9ff
     </ul>
   </section>
 </template>
