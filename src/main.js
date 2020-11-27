308ed470dcd175d30ff5d2e7fb72f86afd610211
Yuval Beit On <yuvalbeiton@gmail.com>
2020-11-27 13:50:55 +0200

    fffffffffff

--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ import router from './router'
 import store from './store'
 import './styles/styles.scss'
 
+// Vue.use(ElementUI, { locale })
+
+
+import lang from 'element-ui/lib/locale/lang/en';
+import locale from 'element-ui/lib/locale';
+import {DatePicker} from 'element-ui';
+Vue.use(DatePicker);
+locale.use(lang)
+
+
 Vue.config.productionTip = false
 
 new Vue({
