e3626c126351ad1dc66a640ed6789010222f4480
Yuval Beit On <yuvalbeiton@gmail.com>
2020-12-01 12:47:14 +0200

    login signup

--- src/views/app-login.vue
+++ src/views/app-login.vue
@@ -1,38 +1,44 @@
 <template>
-  <section class="app-login flex column">
-    <div class="login-header">TaskU LOGO</div>
-    <div class="flex column justify-center">
-      <h1 class="login-title">Log <span class="thin-fonted">In</span></h1>
-      <form
-        class="flex column justify-center align-center"
-        @submit.prevent="submitLogin"
-      >
-        <el-input
-          type="text"
-          v-model="loginCredentials.email"
-          placeholder="Email"
-        />
-        <br />
-        <el-input
-          type="password"
-          v-model="loginCredentials.password"
-          placeholder="Password"
-        />
-        <br />
-        <button class="login-btn" @click="submitLogin">Login</button>
-      </form>
-      <div class="signup-section flex align-center">
-        <span class="separator-line"></span>
-        <div class="login-subtitle flex align-center justify-center">
-          Don't have a user yet?
-
-          <router-link to="/signup">
-            <span class="btn-content"> Signup</span>
-          </router-link>
-        </div>
-        <span class="separator-line"></span>
-      </div>
+  <section class="app-login-container">
+    <div class="login-header">
+      <router-link to="/"
+        ><img class="logo2 flex end" src="@/assets/imgs/logo2.png" alt=""
+      /></router-link>
     </div>
+    <section class="app-login flex column">
+      <div class="flex column justify-center">
+        <h1 class="login-title">Log <span class="thin-fonted">In</span></h1>
+        <form
+          class="flex column justify-center align-center"
+          @submit.prevent="submitLogin"
+        >
+          <el-input
+            type="text"
+            v-model="loginCredentials.email"
+            placeholder="Email"
+          />
+
+          <el-input
+            type="password"
+            v-model="loginCredentials.password"
+            placeholder="Password"
+          />
+
+          <button class="login-btn">Login</button>
+        </form>
+        <div class="signup-section flex align-center">
+          <span class="separator-line"></span>
+          <div class="login-subtitle flex align-center justify-center">
+            Don't have a user yet?
+
+            <router-link to="/signup">
+              <span class="btn-content"> Signup</span>
+            </router-link>
+          </div>
+          <span class="separator-line"></span>
+        </div>
+      </div>
+    </section>
   </section>
 </template>
 
--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -1,90 +1,88 @@
 <template>
-    <section class="home-page">
-        <div class="nav-container flex wrap align-center space-between">
-            <div class="app-logo">TaskU</div>
-            <nav class="nav-home-page flex space-between">
-                <router-link to="/">home</router-link>
-                <router-link v-if="!loggedInUser" to="/login"
-                    >login</router-link
-                >
-                <router-link v-if="!loggedInUser" to="/signup"
-                    >sign up</router-link
-                >
-                <span v-if="loggedInUser" @click="onLogout">logout</span>
-            </nav>
-        </div>
+  <section class="home-page">
+    <div class="nav-container flex wrap align-center space-between">
+      <div class="app-logo">
+        <img class="logo1 flex end" src="@/assets/imgs/logo2.png" alt="" />
+      </div>
+      <nav class="nav-home-page flex space-between">
+        <router-link to="/">home</router-link>
+        <router-link v-if="!loggedInUser" to="/login">login</router-link>
+        <router-link v-if="!loggedInUser" to="/signup">sign up</router-link>
+        <span v-if="loggedInUser" @click="onLogout">logout</span>
+        <button @click="onGetStarted" class="btn-app">
+         Get Started
+        </button>
+      </nav>
+    </div>
 
-        <main class="main-content top-hero">
-            <section class="hero-homepage hero-container">
-                <div class="main-hero-contant flex space-between">
-                    <div
-                        class="content-container flex column align-center wrap"
-                    >
-                        <h1>Team-work in modern days is EASY</h1>
-                        <p>
-                            Lorem ipsum, dolor sit amet consectetur adipisicing
-                            elit. Nesciunt dolore modi numquam nostrum
-                            voluptatem deleniti harum dicta odio eveniet
-                            adipisci magni cupiditate doloribus quae mollitia
-                            soluta itaque nemo, deserunt quia!
-                        </p>
-                        <button @click="onGetStarted" class="btn-app">
-                            {{ enteryButton }}
-                        </button>
-                    </div>
-                    <div class="img-hero">
-                        <img
-                            src="@/assets/imgs/img-hero1.svg"
-                            class="img-home-page"
-                            alt=""
-                            srcset=""
-                        />
-                    </div>
-                </div>
-            </section>
-        </main>
-    </section>
+    <main class="main-content top-hero">
+      <section class="hero-homepage hero-container">
+        <div class="main-hero-contant flex space-between">
+          <div class="content-container flex column align-center wrap">
+            <h1>Team-work in modern days is EASY</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
+              dolore modi numquam nostrum voluptatem deleniti harum dicta odio
+              eveniet adipisci magni cupiditate doloribus quae mollitia soluta
+              itaque nemo, deserunt quia!
+            </p>
+            <button @click="onGetStarted" class="btn-app">
+              {{ enteryButton }}
+            </button>
+          </div>
+          <div class="img-hero">
+            <img
+              src="@/assets/imgs/img-hero1.svg"
+              class="img-home-page"
+              alt=""
+              srcset=""
+            />
+          </div>
+        </div>
+      </section>
+    </main>
+  </section>
 </template>
 
 <script>
 export default {
-    name: 'home-page',
-    computed: {
-        firstBoardId() {
-            const boards = this.$store.getters.boards
-            return boards[0]._id
-        },
-        loggedInUser() {
-            return this.$store.getters.user
-        },
-        enteryButton() {
-            return this.loggedInUser ? 'Enter Workspace' : 'Try as a guest'
-        },
+  name: 'home-page',
+  computed: {
+    firstBoardId() {
+      const boards = this.$store.getters.boards
+      return boards[0]._id
     },
-    methods: {
-        async onGetStarted() {
-            if (this.loggedInUser) {
-                this.$router.push('/board/' + this.firstBoardId)
-            } else {
-                await this.$store.dispatch({
-                    type: 'login',
-                    userCred: 'guest',
-                })
-                await this.$store.dispatch({ type: 'loadBoards' })
-                this.$router.push('/board/' + this.firstBoardId)
-            }
-        },
-        onLogout() {
-            console.log('Logging out')
-            this.$store.dispatch({
-                type: 'logout',
-            })
-        },
+    loggedInUser() {
+      return this.$store.getters.user
     },
-    created() {
-        if (this.loggedInUser) {
-            this.$store.dispatch({ type: 'loadBoards' })
-        }
+    enteryButton() {
+      return this.loggedInUser ? 'Enter Workspace' : 'Try as a guest'
+    }
+  },
+  methods: {
+    async onGetStarted() {
+      if (this.loggedInUser) {
+        this.$router.push('/board/' + this.firstBoardId)
+      } else {
+        await this.$store.dispatch({
+          type: 'login',
+          userCred: 'guest'
+        })
+        await this.$store.dispatch({ type: 'loadBoards' })
+        this.$router.push('/board/' + this.firstBoardId)
+      }
     },
+    onLogout() {
+      console.log('Logging out')
+      this.$store.dispatch({
+        type: 'logout'
+      })
+    }
+  },
+  created() {
+    if (this.loggedInUser) {
+      this.$store.dispatch({ type: 'loadBoards' })
+    }
+  }
 }
 </script>
