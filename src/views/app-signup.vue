b4cf319edcfa19c645a730a0d5997dbe7bda4115
Yuval Beit On <yuvalbeiton@gmail.com>
2020-12-01 11:34:46 +0200

    kdk

--- src/views/app-signup.vue
+++ src/views/app-signup.vue
@@ -1,89 +1,79 @@
 <template>
-    <section class="app-login flex column">
-        <div class="login-header flex">TaskU LOGO</div>
-        <div class="page-content flex align-center justify-center">
-            <div class="login-box w100 flex column align-center">
-                <h1 class="login-title">
-                    Sign <span class="thin-fonted">Up</span>
-                </h1>
-                <div class="login-options-wrapper flex column">
-                    <form @submit.prevent="submitSignup">
-                        <label>
-                            Full name
-                            <input
-                                v-model="signupCredentials.fullName"
-                                type="text"
-                                autocomplete="new-fullname"
-                            />
-                        </label>
-                        <label>
-                            Email
-                            <input
-                                v-model="signupCredentials.email"
-                                type="text"
-                                autocomplete="new-email"
-                            />
-                        </label>
-                        <label>
-                            Password
-                            <input
-                                v-model="signupCredentials.password"
-                                type="password"
-                                autocomplete="new-password"
-                            />
-                        </label>
-                        <button class="submit-button">
-                            Sign Up <i class="fa fa-arrow-right"></i>
-                        </button>
-                    </form>
-                    <span>Already signed up? <router-link to="/login">Let's log-in!</router-link></span>
-
-                </div>
-            </div>
+  <section class="app-signup app-login flex column">
+    <div class="login-header flex">TaskU LOGO</div>
+    <div class="flex column justify-center">
+      <h1 class="login-title">Sign <span class="thin-fonted">up</span></h1>
+      <form
+        class="flex column justify-center align-center"
+        @submit.prevent="submitSignup"
+      >
+        <el-input
+          type="text"
+          v-model="signupCredentials.email"
+          placeholder="Email"
+        />
+        <br />
+        <el-input
+          type="password"
+          v-model="signupCredentials.password"
+          placeholder="Password"
+        />
+        <br />
+        <button class="login-btn" @click="submitLogin">Login</button>
+      </form>
+      <div class="signup-section flex align-center">
+        <span class="separator-line"></span>
+        <div class="login-subtitle flex align-center justify-center">
+          Already have a user?
+          <router-link to="/login">
+            <span class="btn-content"> Login</span>
+          </router-link>
         </div>
-    </section>
+        <span class="separator-line"></span>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-// To DO - Change it's template and css to a SIGNUP one, not login one. TEST ONLY
 import { boardService } from '@/services/board.service'
 
 export default {
-    name: 'app-signup',
-    data() {
-        return {
-            signupCredentials: {
-                fullName: null,
-                email: null,
-                password: null,
-                isAdmin: false,
-            },
-        }
-    },
-    computed: {},
-    methods: {
-        async submitSignup() {
-            const userCred = JSON.parse(JSON.stringify(this.signupCredentials))
-            const user = await this.$store.dispatch({
-                type: 'signup',
-                userCred,
-            })
-            this.signupCredentials = {
-                email: null,
-                password: null,
-                fullName: null,
-                isAdmin: false,
-            }
-            console.log(user)
-            const board = boardService.getEmptyBoard()
-            board.creator = user
-            board.members.push(user)
-            const boardId = await this.$store.dispatch({
-                type: 'saveBoard',
-                board
-            })
-            this.$router.push(`/board/${boardId}`)
-        },
-    },
+  name: 'app-signup',
+  data() {
+    return {
+      signupCredentials: {
+        fullName: null,
+        email: null,
+        password: null,
+        isAdmin: false
+      }
+    }
+  },
+  computed: {},
+  methods: {
+    async submitSignup() {
+      const userCred = JSON.parse(JSON.stringify(this.signupCredentials))
+      const user = await this.$store.dispatch({
+        type: 'signup',
+        userCred
+      })
+      this.signupCredentials = {
+        email: null,
+        password: null,
+        fullName: null,
+        isAdmin: false
+      }
+      console.log(user)
+      const board = boardService.getEmptyBoard()
+      board.creator = user
+      board.members.push(user)
+      const boardId = await this.$store.dispatch({
+        type: 'saveBoard',
+        board
+      })
+      this.$router.push(`/board/${boardId}`)
+    }
+  }
 }
 </script>
